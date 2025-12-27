72beb81ea687b697dcaabf7ad8522ebde2173a44
sanjeet kumar <114237470+sanjeetkumar88@users.noreply.github.com>
2025-12-27 22:42:23 +0530

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import { SessionProvider } from './components/SessionProvider';
 import { Toaster } from 'react-hot-toast';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
+
 
 export const metadata = {
   title: "Book Trading Club",
@@ -22,7 +16,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SessionProvider>
           {children}
